4f5d2ef286fba2bd115383c517ac13abc3700f79
Cristian Diaz <xtndiaz@gmail.com>
2022-08-06 16:39:58 -0500

    Add more Edge Insets extensions

--- Sources/Beryllium/Extensions/EdgeInsetsExtensions.swift
+++ Sources/Beryllium/Extensions/EdgeInsetsExtensions.swift
@@ -20,11 +20,35 @@ extension EdgeInsets {
         .init(top: top, leading: 0, bottom: bottom, trailing: 0)
     }
     
+    public init(_ all: CGFloat) {
+        self.init(horizontal: all, vertical: all)
+    }
+    
     public init(horizontal: CGFloat = 0, vertical: CGFloat = 0) {
         self.init(top: vertical, leading: horizontal, bottom: vertical, trailing: horizontal)
     }
     
     public init(top: CGFloat, horizontal: CGFloat, bottom: CGFloat) {
         self.init(top: top, leading: horizontal, bottom: bottom, trailing: horizontal)
+    }
+    
+    public init(vertical: CGFloat, leading: CGFloat, trailing: CGFloat) {
+        self.init(top: vertical, leading: leading, bottom: vertical, trailing: trailing)
+    }
+    
+    public init(top: CGFloat) {
+        self.init(top: top, leading: 0, bottom: 0, trailing: 0)
+    }
+    
+    public init(leading: CGFloat) {
+        self.init(top: 0, leading: leading, bottom: 0, trailing: 0)
+    }
+    
+    public init(bottom: CGFloat) {
+        self.init(top: 0, leading: 0, bottom: bottom, trailing: 0)
+    }
+    
+    public init(trailing: CGFloat) {
+        self.init(top: 0, leading: 0, bottom: 0, trailing: trailing)
     }
 }
