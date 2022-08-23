0d2c4ca4161058048d2f520c4a9a2574103f3860
Cristian Diaz <xtndiaz@gmail.com>
2022-08-23 11:13:55 -0500

    Add Line Shape

---
+++ Sources/Beryllium/Shapes/Line.swift
@@ -0,0 +1,22 @@
+//
+//  Line.swift
+//  Beryllium
+//
+//  Created by Cristian Diaz on 23.8.2022.
+//
+
+import Foundation
+import SwiftUI
+
+public struct Line: Shape {
+    
+    public init() {
+    }
+    
+    public func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: rect.width, y: 0))
+        return path
+    }
+}
