a25fc91d0cd81124221c13e95facd76b6069fb62
Cristian Diaz <xtndiaz@gmail.com>
2022-09-01 16:44:57 -0500

    Add Date extensions

--- Sources/Beryllium/Extensions/DateTimeExtensions.swift
+++ Sources/Beryllium/Extensions/DateTimeExtensions.swift
@@ -13,3 +13,20 @@ extension TimeInterval {
         UInt64(self * 1_000_000_000)
     }
 }
+
+extension Date {
+    
+    public func components(
+        _ components: Calendar.Component...,
+        inCalendar calendar: Calendar = .current
+    ) -> DateComponents {
+        calendar.dateComponents(Set(components), from: self)
+    }
+    
+    public func component(
+        _ component: Calendar.Component,
+        inCalendar calendar: Calendar = .current
+    ) -> Int {
+        calendar.component(component, from: self)
+    }
+}
